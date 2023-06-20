dc296c6896e02507316994366de7a7bb6829c06d
VictorMFL <vm859222@gmail.com>
2023-06-19 23:25:05 -0300

    add Página de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,120 @@
 'use client'
 
-import { TextField } from '@mui/material'
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 
-export default function Home() {
+import axios from 'axios'
+
+import styles from './LoginPage.module.css'
+
+// Componentes do Material UI
+import { Container, Box, TextField, Button } from '@mui/material'
+
+// Icone
+import { BsPerson } from 'react-icons/bs'
+
+// Rotas
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+// Props da API
+import { ClientProps } from '../interface/interface'
+
+export default function LoginPage() {
+  const [user, setUser] = useState('')
+  const [erro, setErro] = useState<boolean | null>(null)
+  const [data, setData] = useState<ClientProps[]>([])
+
+  const router = useRouter()
+
+  function handleChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setUser(target.value)
+    setErro(null)
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const filteredData = data.filter((item) => item.nome === user)
+    if (filteredData.length === 0) {
+      setErro(true)
+    } else {
+      router.push('/home')
+    }
+  }
+
+  async function get() {
+    try {
+      const response = await axios.get(
+        'https://api-deslocamento.herokuapp.com/api/v1/Cliente',
+      )
+      setData(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    get()
+  }, [])
+
   return (
-    <main>
-      <TextField label="Meu campo de texto Material-UI" />
-    </main>
+    <div className={styles.bg}>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh',
+        }}
+      >
+        <Box
+          sx={{
+            width: 400,
+            height: 300,
+            backgroundColor: '#ffffff',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            borderRadius: '20px',
+            padding: '10px',
+            position: 'relative',
+          }}
+        >
+          <h1>Login</h1>
+          <p style={{ textAlign: 'center' }}>
+            Para fazer login basta colocar seu nome de usuário
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.container_input}>
+              <BsPerson size={32} />
+              <TextField
+                id="outlined-basic"
+                label="Nome"
+                variant="outlined"
+                required
+                onChange={handleChange}
+                value={user}
+              />
+              {erro && <p className={styles.error}>Usuário não encontrado</p>}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '1rem',
+              }}
+            >
+              <Button variant="contained" type="submit">
+                Login
+              </Button>
+            </div>
+          </form>
+          <p className={styles.register}>
+            Não possui uma conta? <Link href="/cadastro">Registre-se</Link>
+          </p>
+        </Box>
+      </Container>
+    </div>
   )
 }
---
+++ src/interface/interface.ts
@@ -0,0 +1,39 @@
+export interface ClientProps {
+  id: number
+  numeroDocumento: string
+  tipoDocumento: string
+  nome: string
+  logradouro: string
+  numero: string
+  bairro: string
+  cidade: string
+  uf: string
+}
+
+export interface ConductorProps {
+  id: number
+  numeroHabilitacao: string
+  categoriaHabilitacao: string
+  vencimentoHabilitacao: string
+}
+
+export interface DisplacementProps {
+  id: number
+  kmInicial: number
+  kmFinal: number
+  inicioDeslocamento: string
+  fimDeslocamento: string
+  checkList: string
+  motivo: string
+  observacao: string
+  idCondutor: number
+  idVeiculo: number
+  idCliente: number
+}
+
+export interface VehicleProps {
+  id: number
+  marcaModelo: string
+  anoFabricacao: string
+  kmAtual: number
+}
